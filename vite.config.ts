29d9f29a4b8f287ac2418ec9b1c4d8ba724ed03e
jose Angel <josegallardo.cor@gmail.com>
2026-01-26 18:39:37 -0600

    feat: configuracion de la herramiento de testing en el  proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
@@ -10,5 +10,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
   },
 })
